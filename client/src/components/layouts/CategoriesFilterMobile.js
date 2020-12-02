45fd36baf163d957ccc19db57940f69cce4fbfb4
HAKIM92-7 <barouni.hakim@gmail.com>
2020-12-02 19:00:08 +0100

    categories for mobile 02

--- client/src/components/layouts/CategoriesFilterMobile.js
+++ client/src/components/layouts/CategoriesFilterMobile.js
@@ -8,12 +8,14 @@ const categories =['Industrie' , 'Autos' , 'Sport' ,'Cuisine', 'Décoration' , '
         <Fragment>
 
 
-<ul className="list-group list-group-horizontal" style={{display:'flex' , flexWrap:'wrap' , marginLeft:'50px' 
+<ul className="list-group list-group-horizontal nav-pills" style={{display:'flex' , flexWrap:'wrap' , marginLeft:'50px' 
 , marginBottom:'20px'}}>
-  <li className="list-group-item"> <a href="#" onClick={()=> props.setCategory('')}>Tous</a></li>
+  <li className="list-group-item" style={{border:'1px grey solid'}}> <a className="nav-link active" id={`v-pills-clear-tab`} data-toggle="pill" href='#v-pills-clear' role="tab" 
+      aria-controls='v-pills-clear 'aria-selected="true" onClick={()=> props.setCategory('')}>Tous</a></li>
 
   {categories.map(category => (
-  <li className="list-group-item"><a href="#" onClick={()=> props.setCategory(category)}>{category}</a></li>
+  <li className="list-group-item" style={{border:'1px grey solid'}}><a className="nav-link" id={`v-pills-${category}-tab`} data-toggle="pill" href={`#v-pills-${category}`} role="tab" 
+  aria-controls={`v-pills-${category}`} aria-selected="true" onClick={()=> props.setCategory(category)}>{category}</a></li>
   ))}
 </ul>
 
